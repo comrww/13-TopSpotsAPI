889b5e74d7b1919b00928a29c5681a9db4e0d41c
Ryan Wilson <comrww@gmail.com>
2016-08-25 16:48:02 -0700

    final commit

--- 13-TopSpotsAPI/13-TopSpotsAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_13_TopSpotsAPI.WebApiApplication" Language="C#" %>
---
+++ TopSpots.API/TopSpots.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace TopSpots.API
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Enables CORS
+            var policy = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(policy);
+
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // This tells Web API to convert normal case to pascal case
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
